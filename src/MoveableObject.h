2024aa968a6e463c85f772d4ac3a91d022f09f35
falconlee236 <falconlee236@naver.com>
2020-11-29 11:43:30 +0900

    define moveableobject

---
+++ src/MoveableObject.h
@@ -0,0 +1,27 @@
+#ifndef MOVEABLEOBJECT_H
+#define MOVEABLEOBJECT_H
+
+#include "GameObject.h"
+
+class MoveableObject : GameObject{
+
+private:
+	float velocity_x;
+	float velocity_y;
+
+public:
+	MoveableObject();
+	MoveableObject(float x, float y);
+
+public:
+	float GetVelocityX();
+	float GetVelocityY();
+	float GetSpeed();
+	void SetVelocityX();
+	void SetVeloctiyY();
+	void SetDirection(int direction);
+	void SetSpeedByFactor(float factor);
+	void Stop();
+};
+
+#endif
