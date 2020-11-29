cc38aa0e1f6abaacecc39a43ee34daf20766741a
falconlee236 <falconlee236@naver.com>
2020-11-29 13:37:40 +0900

    define Health.h

---
+++ src/Health.h
@@ -0,0 +1,15 @@
+#ifndef HEALTH_H
+#define HEALTH_H
+
+#include "Item.h"
+
+class Health : public Item {
+
+public:
+	Health();
+	Health(float x, float y);
+
+public:
+	void HitBy(GameObject& object);
+};
+#endif
