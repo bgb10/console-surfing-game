7d82137526cc3785aa1b1505f4adf6716948a3ab
falconlee236 <falconlee236@naver.com>
2020-11-29 13:39:27 +0900

    define Boost.h

---
+++ src/Boost.h
@@ -0,0 +1,15 @@
+#ifndef BOOST_H
+#define BOOST_H
+
+#include "Item.h"
+
+class Boost : public Item {
+	
+public:
+	Boost();
+	Boost(float x, float y);
+
+public:
+	void HitBy(GameObject& object);
+};
+#endif
