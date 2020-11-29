cd357a3c6c46cb949b6001716dd9f14f1dfdd271
falconlee236 <falconlee236@naver.com>
2020-11-29 12:59:06 +0900

    define Player.h

--- src/MoveableObject.h
+++ src/MoveableObject.h
@@ -3,7 +3,7 @@
 
 #include "GameObject.h"
 
-class MoveableObject : GameObject{
+class MoveableObject : public GameObject{
 
 private:
 	float velocity_x;
---
+++ src/Playerh.h
@@ -0,0 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "MoveableObject.h"
+#include "GameObject.h"
+
+class Player : public MoveableObject{
+	
+private:
+	float invincible_start_x;
+	float invincible_start_y;
+	float invincible_distance;
+
+public:
+	Player();
+	Player(float x, float y);
+
+public:
+	void Move();
+	bool HasIntersected(GameObject& object);
+	void HitBy(GameObject& object);
+	void SetInvincibleDistance(float distance);
+};
+
+#endif
