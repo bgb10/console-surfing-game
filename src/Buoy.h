bce0b747f9de137bd20e3c28da3bfe347d31e115
falconlee236 <falconlee236@naver.com>
2020-11-29 13:33:48 +0900

    define Buoy.h

---
+++ src/Buoy.h
@@ -0,0 +1,15 @@
+#ifndef BUOY_H
+#define BUOY_H
+
+#include "Obstacle.h"
+
+class Buoy : public Obstacle {
+	
+public:
+	Buoy();
+	Buoy(float x, float y);
+
+public:
+	void HitBy(GameObject& object);
+};
+#endif
