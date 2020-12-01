d277ae431f21a66e1356427fe06874cff2fbc596
bgb10 <bgb10@naver.com>
2020-12-01 16:35:06 +0900

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include ... // include required components
+
+// global variables here
+
+int main()
+{
+    GameManager game; // handles everything (user input, rendering, etc.) with initialization
+
+    return 0;
+}
