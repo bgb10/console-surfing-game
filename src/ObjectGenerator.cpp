88fca928af742478c7326ed70779d9dde64cfe4f
BERICT-PC <dev.bedrock@gmail.com>
2020-12-03 04:45:01 +0900

    Resolve #13
    Fixed typos in ObjectGenerator.cpp
    Updated LevelUp()

--- src/ObjectGenerator.cpp
+++ src/ObjectGenerator.cpp
@@ -1,88 +1,90 @@
 #include "ObjectGenerator.h"
 
-void ObjectGenerator::Generate(ObjectManager& objectManager, SceneManager& sceneManager){
-    Deck deck;
-    Buoy byou;
-    Kraken kraken;
+void ObjectGenerator::Generate(ObjectManager& objectManager) {
+	// set the default obstacles
+	// objectManager.AddImmovable(Boost::Boost(0.0f, 2.0f));
+	// ... add more here
+}
 
-    Boost boost; 
-    Health health;
+void ObjectGenerator::Generate(ObjectManager& objectManager, SceneManager& sceneManager) {
+	Deck deck;
+	Buoy buoy;
+	Kraken kraken;
 
-    random_device rd;
-    mt19937 gen(rd());
-    //Scene x:160 y:45 
-    uniform_int_distribution<float> x_pos(-sceneManager.GetWidth()/2, -sceneManager.GetWidth()/2); //-80<x_pos<80 
-    srand(time(0));
-    float obstacle_x_pos;
-    int db,hb; //deck,byou - health,boost
+	Boost boost;
+	Health health;
 
-    //Create Obstacles to Match chance_map Probability
-    if(rand()%100<chance_map[GetLevel()-1][2]*100){
-        obstacle_x_pos = x_pos(gen);
-        db = rand()%2;
-        if(db)
-            deck.SetCenter(obstacle_x_pos,-24);
-        else
-            byou.SetCenter(obstacle_x_pos,-24);
-    }
-    /*
-    After the hasintersected test,
-    If it is hasintersected, use 'goto' to setcenter again.
-    */
-    //Create Kraken to Match chance_map Probability
-    if(rand()%100<chance_map[GetLevel()-1][0]*100){
-        K:
-        kraken.SetCenter(x_pos(gen),24);
-        if(db == 1){
-            if(kraken.HasIntersected(deck))
-                goto K;
-        }    
-        else{ 
-            if(kraken.HasIntersected(byou))
-                goto K; 
-        }
-    } 
+	random_device rd;
+	mt19937 gen(rd());
+	//Scene x:160 y:45 
+	uniform_int_distribution<float> x_pos(-sceneManager.GetWidth() / 2, -sceneManager.GetWidth() / 2); //-80<x_pos<80 
+	srand(time(0));
+	float obstacle_x_pos;
+	int db, hb; //deck,buoy - health,boost
 
-    //Create Item to Match chance_map Probability
-    if(rand()%100<chance_map[GetLevel()-1][3]*100){
-        hb = rand()%2;
-        if(hb){
-            h:
-            health.SetCenter(x_pos(gen),-24);
-            if(db){
-                if(health.HasIntersected(deck)||health.HasIntersected(kraken)){
-                    goto h;
-                }
-                else if(health.HasIntersected(byou)||health.HasIntersected(kraken)){
-                    goto h;
-                }
-            }
-        }
-        else{
-            b:
-            boost.SetCenter(x_pos(gen),-24);
-            if(db){
-                if(boost.HasIntersected(deck)||boost.HasIntersected(kraken)){
-                    goto b;
-                }
-                else if(byou.HasIntersected(deck)||byou.HasIntersected(kraken)){
-                    goto b;
-                }
-            }
-        }
-    } 
-    
+	//Create Obstacles to Match chance_map Probability
+	if (rand() % 100 < chance_map[GetLevel() - 1][2] * 100) {
+		obstacle_x_pos = x_pos(gen);
+		db = rand() % 2;
+		if (db)
+			deck.SetCenter(obstacle_x_pos, -24);
+		else
+			buoy.SetCenter(obstacle_x_pos, -24);
+	}
+	/*
+	After the hasintersected test,
+	If it is hasintersected, use 'goto' to setcenter again.
+	*/
+	//Create Kraken to Match chance_map Probability
+	if (rand() % 100 < chance_map[GetLevel() - 1][0] * 100) {
+	K:
+		kraken.SetCenter(x_pos(gen), 24);
+		if (db == 1) {
+			if (kraken.HasIntersected(deck))
+				goto K;
+		}
+		else {
+			if (kraken.HasIntersected(buoy))
+				goto K;
+		}
+	}
+
+	//Create Item to Match chance_map Probability
+	if (rand() % 100 < chance_map[GetLevel() - 1][3] * 100) {
+		hb = rand() % 2;
+		if (hb) {
+		h:
+			health.SetCenter(x_pos(gen), -24);
+			if (db) {
+				if (health.HasIntersected(deck) || health.HasIntersected(kraken)) {
+					goto h;
+				}
+				else if (health.HasIntersected(buoy) || health.HasIntersected(kraken)) {
+					goto h;
+				}
+			}
+		}
+		else {
+		b:
+			boost.SetCenter(x_pos(gen), -24);
+			if (db) {
+				if (boost.HasIntersected(deck) || boost.HasIntersected(kraken)) {
+					goto b;
+				}
+				else if (buoy.HasIntersected(deck) || buoy.HasIntersected(kraken)) {
+					goto b;
+				}
+			}
+		}
+	}
+
 }
 //Getter
-int ObjectGenerator::GetLevel(){
-    return level;
+int ObjectGenerator::GetLevel() {
+	return level;
 }
 
-void ObjectGenerator::LevelUp(){
-    if(level >= 3){
-        //nothing happen
-        level = 3;
-    }
-    else{level = level+1;}//increase one level   
-    //The amount of unit distance should be decided.
+void ObjectGenerator::LevelUp() {
+	//increase one level
+	level = level + 1;
 }
--- src/ObjectGenerator.h
+++ src/ObjectGenerator.h
@@ -4,11 +4,11 @@
 #include "SceneManager.h"
 #include "ObjectManager.h"
 #include "GameObject.h"
-#include "../GameObject/MovableObject/Kraken.h"
-#include "../GameObject/Obstacle/Buoy.h"
-#include "../GameObject/Obstacle/Deck.h"
-#include "../GameObject/Item/Boost.h"
-#include "../GameObject/Item/Health.h"
+#include "GameObject/MovableObject/Kraken.h"
+#include "GameObject/Obstacle/Buoy.h"
+#include "GameObject/Obstacle/Deck.h"
+#include "GameObject/Item/Boost.h"
+#include "GameObject/Item/Health.h"
 
 #include <random> //random
 #include <ctime>
@@ -18,12 +18,13 @@ using namespace std;
 class ObjectGenerator{
     private:
         int level = 0;
-        float chance_map[3][5] = {
+        const float chance_map[3][5] = {
             {0.0, 0.2,  0.1,  0.2,  0.2},
             {0.1, 0.25, 0.2,  0.2,  0.25},
             {0.2, 0.3,  0.25, 0.15, 0.3},
         };
     public:
+        void Generate(ObjectManager& objectManager);
         void Generate(ObjectManager& objectManager, SceneManager& sceneManager);
         int GetLevel();
         void LevelUp();
