3f028e1a8ad303b483ddd194f14d9a0dd7485500
이지학 <jooin2000@naver.com>
2020-12-02 12:14:57 +0900

    Add ObjectGenerator.cpp, ObjectManager.cpp

--- src/ObjectManager.cpp
+++ src/ObjectManager.cpp
@@ -1,5 +1,5 @@
 #include "ObjectManager.h"
-
+#include <string>
 //constructor
 ObjectManager::ObjectManager(){
     
@@ -8,4 +8,35 @@ ObjectManager::ObjectManager(){
 Player ObjectManager::GetPlayer(){
     return m_player;
 }
-vector<MovableObject> GetMovable()
+vector<MovableObject> ObjectManager::GetMovable(){
+    return m_vector_movable;
+}
+vector<GameObject> ObjectManager::GetImmovable(){
+    return m_vector_immovable;
+}
+//Setter
+void ObjectManager::AddMovable(MovableObject object){
+    m_vector_movable.push_back(object); //add Movable object using push_back
+}
+void ObjectManager::AddImmovable(GameObject object){
+    m_vector_immovable.push_back(object);//add Immovable object using push_back
+}
+void ObjectManager::ResetPlayer(){
+    
+}
+void ObjectManager::RemoveMovable(MovableObject object){
+    //find_if
+}
+void ObjectManager::RemoveImmovable(GameObject object){
+    //find_if
+}
+void ObjectManager::RemoveAllMovables(){
+    m_vector_movable.clear();
+}
+void ObjectManager::RemoveAllImmovables(){
+    m_vector_immovable.clear();
+}
+void ObjectManager::Clear(){
+    m_vector_movable.clear();
+    m_vector_immovable.clear();
+}
