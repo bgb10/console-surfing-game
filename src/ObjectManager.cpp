f3ad50ff1723a3abbb9f87b7f9bc74a7d3286606
이지학 <jooin2000@naver.com>
2020-12-03 01:02:53 +0900

    Update ObjectManager.cpp

--- src/ObjectManager.cpp
+++ src/ObjectManager.cpp
@@ -2,8 +2,10 @@
 #include <string>
 //constructor
 ObjectManager::ObjectManager(){
-    
+    ObjectManager ob;
+    m_player.SetCenter(0,0);
 }
+
 //Getter
 Player ObjectManager::GetPlayer(){
     return m_player;
@@ -14,6 +16,7 @@ vector<MovableObject> ObjectManager::GetMovable(){
 vector<GameObject> ObjectManager::GetImmovable(){
     return m_vector_immovable;
 }
+
 //Setter
 void ObjectManager::AddMovable(MovableObject object){
     m_vector_movable.push_back(object); //add Movable object using push_back
@@ -22,7 +25,10 @@ void ObjectManager::AddImmovable(GameObject object){
     m_vector_immovable.push_back(object);//add Immovable object using push_back
 }
 void ObjectManager::ResetPlayer(){
-    
+    m_player.SetDirection(0);
+    m_player.SetVelocityX(); // not decided yet
+    m_player.SetVeloctiyY();
+    m_player.SetCenter(0,0);
 }
 void ObjectManager::RemoveMovable(MovableObject object){
     vector <MovableObject>::iterator it;
@@ -31,12 +37,12 @@ void ObjectManager::RemoveMovable(MovableObject object){
             m_vector_movable.erase(m_vector_movable.begin()+i);
         }
     }
-   //I don'know how can i use std::find_if
+   //I don'know how can i use std::find_if 
 }
 void ObjectManager::RemoveImmovable(GameObject object){
-      for(int i=0;i<m_vector_movable.size();i++){
-        if(m_vector_movable[i].GetID()==object.GetID()){
-            m_vector_movable.erase(m_vector_movable.begin()+i);
+      for(int i=0;i<m_vector_immovable.size();i++){
+        if(m_vector_immovable[i].GetID()==object.GetID()){
+            m_vector_immovable.erase(m_vector_immovable.begin()+i);
         }
     }
 }
