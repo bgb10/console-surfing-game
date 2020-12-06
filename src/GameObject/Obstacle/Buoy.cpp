4464c19384ac2860e8d833e97f363747e0d7c5fb
PEERING\SY Park <foba1@naver.com>
2020-12-06 23:53:06 +0900

    Fix buoy to crash with kraken, player

--- src/GameObject/Obstacle/Buoy.cpp
+++ src/GameObject/Obstacle/Buoy.cpp
@@ -3,6 +3,8 @@
 
 #include <random> // Random
 
+extern int life_count;
+
 // Constructors
 Buoy::Buoy() : Buoy(0, 0) {}
 
@@ -16,6 +18,7 @@ Buoy::Buoy(float x, float y) : Obstacle(x, y)
 
 void Buoy::HitBy(MovableObject* object)
 {
+	/*
 	if (is_collidable)
 	{
 		is_collidable = false;
@@ -46,10 +49,21 @@ void Buoy::HitBy(MovableObject* object)
 			}
 		}
 	}
+	*/
+	if (is_collidable)
+	{
+		is_collidable = false;
+
+		// Actions on collision between Player and Movable are different
+		// stop the object on collision
+		//object->Stop();
+		object->has_collision = true;
+	}
 }
 
 void Buoy::HitBy(Player* player)
 {
+	/*
 	if (is_collidable)
 	{
 		is_collidable = false;
@@ -57,5 +71,20 @@ void Buoy::HitBy(Player* player)
 		// Actions on collision between Player and Movable are the same
 		// call Buoy::HitBy(MovableObject* object)
 		Buoy::HitBy(dynamic_cast<MovableObject*>(player));
+	}
+	*/
+	if (is_collidable)
+	{
+		is_collidable = false;
+
+		// decrease life count
+		if (life_count > 0)
+			life_count--;
+
+		// stop player on collision, resume playing by pressing down arrow
+		player->Stop();
+
+		// make player invincible for 5 in-game unit distances
+		player->SetInvincibleDistance(5.0);
 	}
 }
